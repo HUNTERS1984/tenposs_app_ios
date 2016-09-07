86b8cd887e41c05f100372d97f3c4f708bec85b9
phucnguyen <nguyenhuuphuc08@gmail.com>
2016-09-07 13:13:29 +0700

    Add NSString additional

---
+++ Tenposs/Tenposs/Utils/NSString.h
@@ -0,0 +1,17 @@
+//
+//  NSString.h
+//  VideoFly_beta_10
+//
+//  Created by Phúc Nguyễn on 7/29/15.
+//  Copyright (c) 2015 Phuc Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (videofly)
+
+- (NSString *)stringByUrlEncoding;
+- (NSString * )md5;
+- (NSString*)sha1;
+
+@end
